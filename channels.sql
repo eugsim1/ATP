189254a1056d5bb1bd35a3047a9add61eb80846f
eugsim1 <22279788+eugsim1@users.noreply.github.com>
2019-05-07 11:55:18 +0200

    Update channels.sql

--- channels.sql
+++ channels.sql
@@ -1,6 +1,3 @@
---------------------------------------------------------
---  File created - Tuesday-May-07-2019   
---------------------------------------------------------
 REM INSERTING into ATPC_USER.CHANNELS_1
 SET DEFINE OFF;
 Insert into ATPC_USER.CHANNELS_1 (CHANNEL_ID,CHANNEL_DESC,CHANNEL_CLASS,CHANNEL_CLASS_ID,CHANNEL_TOTAL,CHANNEL_TOTAL_ID) values ('3','Direct Sales','Direct','12','Channel total','1');
